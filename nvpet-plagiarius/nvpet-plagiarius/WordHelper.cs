118f496f4c9518f47e70aeb994eedc4d91eeedec
Vladislav <deimosvsh@gmail.com>
2019-10-02 11:46:08 +0300

    word helper

---
+++ nvpet-plagiarius/nvpet-plagiarius/WordHelper.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace nvpet_plagiarius
+{
+    public class PorterStemmerUkr
+    {
+        private static Regex PERFECTIVEGROUND = new Regex("(ив|ивши|ившись|ів|івши|івшись((?<=[ая|я])(в|вши|вшись)))$");
+
+        private static Regex REFLEXIVE = new Regex("(с[яьи])$");
+
+        private static Regex ADJECTIVE = new Regex("(ими|ій|ий|а|е|ова|ове|ів|є|їй|єє|еє|я|ім|ем|им|ім|их|іх|ою|йми|іми|у|ю|ого|ому|ої)$");
+
+        private static Regex PARTICIPLE = new Regex("((ий|ого|ому|им|ім|а|ій|у|ою|ій|і|их|йми|их))$");
+
+        private static Regex VERB = new Regex("(сь|ся|ив|ать|ять|у|ю|ав|али|учи|ячи|вши|ши|е|ме|ати|яти|є)$");
+
+        private static Regex NOUN = new Regex("(а|ев|ов|е|ями|ами|еи|и|ей|ой|ий|й|иям|ям|ием|ем|ам|ом|о|у|ах|иях|ях|ы|ь|ию|ью|ю|ия|ья|я|і|ові|ї|ею|єю|ою|є|еві|ем|єм|ів|їв|\'ю)$");
+
+        private static Regex RVRE = new Regex("^(.*?[аеиоуюяіїє])(.*)$");
+
+        private static Regex DERIVATIONAL = new Regex("[^аеиоуюяіїє][аеиоуюяіїє]+[^аеиоуюяіїє]+[аеиоуюяіїє].*(?<=о)сть?$");
+
+        private static Regex DER = new Regex("ость?$");
+
+        private static Regex SUPERLATIVE = new Regex("(ейше|ейш)$");
+
+        private static Regex I = new Regex("и$");
+        private static Regex P = new Regex("ь$");
+        private static Regex NN = new Regex("нн$");
+
+        public PorterStemmerUkr()
+        {
+        }
+
+        public static string TransformingWord(string word)
+        {
+            word = word.ToLower();
+            word = word.Replace('ё', 'е');
+            MatchCollection m = RVRE.Matches(word);
+            if (m.Count > 0)
+            {
+                Match match = m[0]; // only one match in this case 
+                GroupCollection groupCollection = match.Groups;
+                string pre = groupCollection[1].ToString();
+                string rv = groupCollection[2].ToString();
+
+                MatchCollection temp = PERFECTIVEGROUND.Matches(rv);
+                string StringTemp = ReplaceFirst(temp, rv);
+
+
+                if (StringTemp.Equals(rv))
+                {
+                    MatchCollection tempRV = REFLEXIVE.Matches(rv);
+                    rv = ReplaceFirst(tempRV, rv);
+                    temp = ADJECTIVE.Matches(rv);
+                    StringTemp = ReplaceFirst(temp, rv);
+                    if (!StringTemp.Equals(rv))
+                    {
+                        rv = StringTemp;
+                        tempRV = PARTICIPLE.Matches(rv);
+                        rv = ReplaceFirst(tempRV, rv);
+                    }
+                    else
+                    {
+                        temp = VERB.Matches(rv);
+                        StringTemp = ReplaceFirst(temp, rv);
+                        if (StringTemp.Equals(rv))
+                        {
+                            tempRV = NOUN.Matches(rv);
+                            rv = ReplaceFirst(tempRV, rv);
+                        }
+                        else
+                        {
+                            rv = StringTemp;
+                        }
+                    }
+
+                }
+                else
+                {
+                    rv = StringTemp;
+                }
+
+                MatchCollection tempRv = I.Matches(rv);
+                rv = ReplaceFirst(tempRv, rv);
+                if (DERIVATIONAL.Matches(rv).Count > 0)
+                {
+                    tempRv = DER.Matches(rv);
+                    rv = ReplaceFirst(tempRv, rv);
+                }
+
+                temp = P.Matches(rv);
+                StringTemp = ReplaceFirst(temp, rv);
+                if (StringTemp.Equals(rv))
+                {
+                    tempRv = SUPERLATIVE.Matches(rv);
+                    rv = ReplaceFirst(tempRv, rv);
+                    tempRv = NN.Matches(rv);
+                    rv = ReplaceFirst(tempRv, rv);
+                }
+                else
+                {
+                    rv = StringTemp;
+                }
+                word = pre + rv;
+
+            }
+
+            return word;
+        }
+
+        public static string ReplaceFirst(MatchCollection collection, string part)
+        {
+            string StringTemp = "";
+            if (collection.Count == 0)
+            {
+                return part;
+            }
+            /*else if(collection.Count == 1) 
+            { 
+            return StringTemp; 
+            }*/
+            else
+            {
+                StringTemp = part;
+                for (int i = 0; i < collection.Count; i++)
+                {
+                    GroupCollection GroupCollection = collection[i].Groups;
+                    if (StringTemp.Contains(GroupCollection[i].ToString()))
+                    {
+                        string deletePart = GroupCollection[i].ToString();
+                        StringTemp = StringTemp.Replace(deletePart, "");
+                    }
+
+                }
+            }
+            return StringTemp;
+        }
+    }
+}
