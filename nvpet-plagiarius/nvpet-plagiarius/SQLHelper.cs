2a74e33055f786a2e7c6b82a90c2876a440e156a
Vladislav <deimosvsh@gmail.com>
2019-10-07 16:10:18 +0300

    0.201

---
+++ nvpet-plagiarius/nvpet-plagiarius/SQLHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace nvpet_plagiarius
+{
+    public class SQLHelper
+    {
+        public SQLHelper()
+        {
+        }
+
+        public string GetFromMotherBase()
+        {
+            string sResult;
+
+
+
+            return sResult;
+        }
+    }
+}
